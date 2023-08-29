5a29141b014c0903af9a23d4fc595f304d0674f0
palat2000 <palat.tungsantitham@gmail.com>
2023-08-29 15:20:57 +0700

    finish-app-layout

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import "./App.scss";
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <div className="todo">
+      <div className="todo__header">Header</div>
+      <div className="todo__sidebar">SideBar</div>
+      <div className="todo__content">TodoContent</div>
+    </div>
+  );
 }
 
 export default App;
