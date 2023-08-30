6e02ea716a9284ab7fa644afc95d57cacf6c7962
palat2000 <palat.tungsantitham@gmail.com>
2023-08-30 17:21:12 +0700

    update:todo list

--- src/components/Todo/TodoLists.jsx
+++ src/components/Todo/TodoLists.jsx
@@ -1,7 +1,24 @@
 import styles from "./TodoLists.module.scss";
+import { FaRegCircle, FaTrashAlt, FaPen } from "react-icons/fa";
 
 function TodoLists() {
-  return <div>TodoList</div>;
+  return (
+    <ul className={styles.todo_lists}>
+      <li className={styles.todo}>
+        <span className={styles.todo__checkbox}>{/* <FaRegCircle /> */}</span>
+        <p className={styles.todo__task}>Todo Item 1</p>
+        <span className={styles.todo__date}>30 AUG</span>
+        <div className={styles.todo__action}>
+          <span className={styles.todo__edit}>
+            <FaPen />
+          </span>
+          <span className={styles.todo__delete}>
+            <FaTrashAlt />
+          </span>
+        </div>
+      </li>
+    </ul>
+  );
 }
 
 export default TodoLists;
