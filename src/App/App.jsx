a2b408e05ac9277e46722ee755b25a6afc165ccd
palat2000 <palat.tungsantitham@gmail.com>
2023-08-31 09:15:50 +0700

    complate from p'v

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,52 +1,23 @@
-import "./App.scss";
-import Header from "../components/Header";
-// import ListItem from "../components/ListItem";
-import List from "../components/List";
-import TodoHeader from "../components/Todo/TodoHeader";
-import TodoCreate from "../components/Todo/TodoCreate";
-import TodoLists from "../components/Todo/TodoLists";
-import {
-  FaInbox,
-  FaCalendar,
-  FaCalendarAlt,
-  FaChevronDown,
-} from "react-icons/fa";
+// Dependencies
+import './App.scss';
+import AppBar from '../components/Common/AppBar/AppBar';
+import SideBar from '../components/SideBar/SideBar';
+import TodoHeader from '../components/Todo/TodoHeader';
+import TodoCreate from '../components/Todo/TodoCreate';
+import TodoLists from '../components/Todo/TodoLists';
+import { Button } from '../components/Common/Button/Button';
+
 function App() {
-  const generalList = [
-    { id: 1, text: "inbox", icon: <FaInbox />, active: true },
-    { id: 2, text: "Today", icon: <FaCalendar />, active: false },
-    { id: 3, text: "Next 7 days", icon: <FaCalendarAlt />, active: false },
-  ];
-  const projectList = [
-    { id: 4, text: "Project-A", icon: <FaInbox />, active: true },
-    { id: 5, text: "Project-B", icon: <FaInbox />, active: false },
-  ];
   return (
-    <div className="todo">
-      <div className="todo__header">
-        <Header />
+    <div className='todo'>
+      <div className='todo__header'>
+        <AppBar />
       </div>
-      <div className="todo__sidebar">
-        <aside className="sidebar">
-          <section className="sidebar__category">
-            <List data={generalList} />
-          </section>
-          <section className="sidebar__category">
-            <div className="accordion">
-              {/* Toggle */}
-              <div className="accordion__toggle">
-                <li className="accordion__item">
-                  <FaChevronDown className="accordion__item__icon accordion__item__active" />
-                  <p className="accordion__item__text">Projects</p>
-                </li>
-              </div>
-              <List data={projectList} />
-            </div>
-          </section>
-        </aside>
+      <div className='todo__sidebar'>
+        <SideBar />
       </div>
-      <div className="todo__content">
-        <main className="todo__container">
+      <div className='todo__content'>
+        <main className='todo__container'>
           <TodoHeader />
           <TodoCreate />
           <TodoLists />
