6e3715b6f3bb4e9a5c35172c3ccfb061cab4ead8
palat2000 <palat.tungsantitham@gmail.com>
2023-08-31 11:04:29 +0700

    condition render todocreate

--- src/components/Todo/TodoCreate.jsx
+++ src/components/Todo/TodoCreate.jsx
@@ -1,15 +1,37 @@
-import styles from './TodoCreate.module.scss';
-import { FaPlus } from 'react-icons/fa';
-import { HiPlus } from 'react-icons/hi';
+import { useState } from "react";
+
+import { FaPlus } from "react-icons/fa";
+import { HiPlus } from "react-icons/hi";
+
+import TodoForm from "./TodoForm";
+import styles from "./TodoCreate.module.scss";
+
+/*
+Condition Rendering
+- Default : Show Button & Text
+- Active : Show TodoForm
+*/
 
 function TodoCreate() {
+  const [isOpenForm, setIsOpenForm] = useState(false);
+
+  const handleClick = (e) => {
+    setIsOpenForm(!isOpenForm);
+  };
+
   return (
-    <div className={styles.todo__create}>
-      <div className={styles.todo__create__button}>
-        <HiPlus />
-      </div>
-      <h3 className={styles.todo__create__text}>Add Task</h3>
-    </div>
+    <>
+      {isOpenForm ? (
+        <TodoForm />
+      ) : (
+        <div onClick={handleClick} className={styles.todo__create}>
+          <div className={styles.todo__create__button}>
+            <HiPlus />
+          </div>
+          <h3 className={styles.todo__create__text}>Add Task</h3>
+        </div>
+      )}
+    </>
   );
 }
 
